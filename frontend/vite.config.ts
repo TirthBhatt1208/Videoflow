a274c3d9a4c8b18b13740768039e73613c452b8e
Tirth Bhatt <tirth.bhatt1208@gmail.com>
2026-03-06 20:37:07 +0530

    feat: update Vite configuration to load environment variables for API proxy target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://backend:8000",
-        changeOrigin: true,
-        configure: (proxy, options) => {
-          proxy.on("error", (err, req, res) => {
-            console.log("🔴 Proxy error:", err);
-          });
-          proxy.on("proxyReq", (proxyReq, req, res) => {
-            console.log(
-              "🚀 Proxying:",
-              req.method,
-              req.url,
-              "→",
-              options.target,
-            );
-          });
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            console.log("✅ Proxy response:", proxyRes.statusCode, req.url);
-          });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [tailwindcss(), react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_CORS_ORIGIN,
+          changeOrigin: true,
+          configure: (proxy, options) => {
+            proxy.on("error", (err, req, res) => {
+              console.log("🔴 Proxy error:", err);
+            });
+            proxy.on("proxyReq", (proxyReq, req, res) => {
+              console.log(
+                "🚀 Proxying:",
+                req.method,
+                req.url,
+                "→",
+                options.target,
+              );
+            });
+            proxy.on("proxyRes", (proxyRes, req, res) => {
+              console.log("✅ Proxy response:", proxyRes.statusCode, req.url);
+            });
+          },
         },
       },
     },
-  },
+  };
 });
